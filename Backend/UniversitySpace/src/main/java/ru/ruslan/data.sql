75285f0acaf46e7bb5ab18ca2f9da5fdc7edf44e
Ruslan815 <ruslansaf2000@gmail.com>
2022-01-31 22:10:22 +0700

    Merged sources of web-security and web-socket projects

---
+++ Backend/UniversitySpace/src/main/java/ru/ruslan/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO T_ROLE(id, name) VALUES (0, 'ROLE_ADMIN'), (1, 'ROLE_USER');
+
+INSERT INTO T_USER(id, username, password) VALUES (1, 'admin', '$2a$12$STs9BW4ICmVK.a676bai5OwG7H4HA/XdmaKmoqXGh6YdMeU30bau.'), (2, 'cat', '$2a$12$YT/5vraodocUVG.mwlaAP.3iL7czqNIo547JM.5GLVT5bjSEF4dPq');
+
+INSERT INTO T_USER_ROLES(user_id, roles_id) VALUES (1, 0), (2, 1);
