fa0381244bd02a2e6e30437cd10782b0ce1fe2a4
Ruslan815 <ruslansaf2000@gmail.com>
2022-01-31 20:38:43 +0700

    Deleted all files from repository

--- src/main/java/ru/ruslan/diploma/chat/repository/ChatRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.ruslan.diploma.chat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.ruslan.diploma.chat.model.Chat;
-
-@Repository
-public interface ChatRepository extends JpaRepository<Chat, Integer> {
-}
