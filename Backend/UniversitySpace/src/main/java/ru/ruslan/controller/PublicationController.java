785cbe0dc8188ff8bf11656f67dae9e7b0fae766
Ruslan815 <ruslansaf2000@gmail.com>
2022-04-06 13:04:13 +0700

    Added example of getting username on server and on webpage.

--- Backend/UniversitySpace/src/main/java/ru/ruslan/controller/PublicationController.java
+++ Backend/UniversitySpace/src/main/java/ru/ruslan/controller/PublicationController.java
@@ -1,15 +1,26 @@
 package ru.ruslan.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.ruslan.entity.SecurityUser;
 
 @Controller
 public class PublicationController {
 
     @GetMapping("/publication")
     public String getAllPublications(@RequestParam String pageNumber) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String username;
+        if (principal instanceof SecurityUser) {
+            username = ((SecurityUser) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        System.out.println(username);
         return "html/test";
     }
 
