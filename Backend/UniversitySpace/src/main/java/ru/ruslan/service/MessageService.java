2329c68ed5a145cf5964d1aea682e54a193d23ab
Ruslan815 <ruslansaf2000@gmail.com>
2022-04-15 19:28:45 +0700

    Alfa version of new readMessages method

--- Backend/UniversitySpace/src/main/java/ru/ruslan/service/MessageService.java
+++ Backend/UniversitySpace/src/main/java/ru/ruslan/service/MessageService.java
@@ -2,6 +2,7 @@ package ru.ruslan.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
@@ -62,40 +63,69 @@ public class MessageService {
         return responseList;
     }
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE)
     public synchronized List<MessageView> readMessages(User someUser, Integer chatId) {
-        //List<Message> chatMessagesList = messageRepository.findAllByChatId(chatId, Sort.by(Sort.Direction.DESC, "sendTime"));
-        List<Message> chatMessagesList = unreadMessages.get(chatId);
+        // // // List<Message> chatMessagesList = messageRepository.findAllByChatId(chatId, Sort.by(Sort.Direction.DESC, "sendTime"));
+        //List<Message> chatMessagesList = unreadMessages.get(chatId);
 
-        System.out.println(Thread.currentThread().getId() + ") START LIST");
+        /*System.out.println(Thread.currentThread().getId() + ") START LIST");
         for (Message someMessage : chatMessagesList) {
             System.out.print(Thread.currentThread().getId() + ") " + someMessage.getMessageId() + " : ");
             for (User tempUser : someMessage.getUsersWhoDidNotRead()) {
                 System.out.print(tempUser.getId() + ", ");
             }
             System.out.println();
+        }*/
+
+        // SELECT * FROM MESSAGES WHERE MESSAGE_ID in (SELECT UNREAD_MESSAGES_MESSAGE_ID FROM MESSAGES_USERS_WHO_DID_NOT_READ where USERS_WHO_DID_NOT_READ_ID = 1);
+        String queryText1 = "SELECT * FROM MESSAGES WHERE CHAT_ID = " + chatId + " MESSAGE_ID in ";
+        String queryText2 = "(SELECT UNREAD_MESSAGES_MESSAGE_ID FROM MESSAGES_USERS_WHO_DID_NOT_READ where USERS_WHO_DID_NOT_READ_ID = " + someUser.getId() + ");";
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(queryText1 + queryText2);
+        List<MessageView> responseList = new ArrayList<>();
+        List<Integer> messageIdForDelete = new ArrayList<>();
+        for (Map<String, Object> row : rows) {
+            Message queryMessage = new Message();
+
+            queryMessage.setMessageId((Integer) row.get("MESSAGE_ID"));
+            messageIdForDelete.add((Integer) row.get("MESSAGE_ID"));
+
+            queryMessage.setUserId((Integer) row.get("USER_ID"));
+            queryMessage.setText((String) row.get("TEXT"));
+            queryMessage.setSendTime((String) row.get("SEND_TIME"));
+
+            System.out.println(queryMessage.getMessageId() + " | " + queryMessage.getUserId() + " | " + queryMessage.getText() + " | " + queryMessage.getSendTime());
+            responseList.add(new MessageView(queryMessage));
         }
 
-        List<MessageView> responseList = new ArrayList<>();
+        if (!messageIdForDelete.isEmpty()) {
+            queryText1 = "delete FROM MESSAGES_USERS_WHO_DID_NOT_READ where UNREAD_MESSAGES_MESSAGE_ID in " +
+                    messageIdForDelete.toString().replace("[", "(").replace("]", ")");
+            jdbcTemplate.execute(queryText1);
+        }
+
+        /*List<MessageView> responseList = new ArrayList<>();
         for (Message someMessage : chatMessagesList) {
             if (someMessage.getUsersWhoDidNotRead().contains(someUser)) {
                 someMessage.getUsersWhoDidNotRead().remove(someUser);
                 messageRepository.saveAndFlush(someMessage);
                 responseList.add(new MessageView(someMessage));
             }
-        }
-        unreadMessages.put(chatId, chatMessagesList);
+        }*/
+        //unreadMessages.put(chatId, chatMessagesList);
 
-        System.out.println(Thread.currentThread().getId() + ") END LIST");
+        /*System.out.println(Thread.currentThread().getId() + ") END LIST");
         for (Message someMessage : unreadMessages.get(chatId)) {
             System.out.print(Thread.currentThread().getId() + ") " + someMessage.getMessageId() + " : ");
             for (User tempUser : someMessage.getUsersWhoDidNotRead()) {
                 System.out.print(tempUser.getId() + ", ");
             }
             System.out.println();
-        }
+        }*/
 
-        System.out.println(Thread.currentThread().getId() + ") FETCHED LIST");
+        /*System.out.println(Thread.currentThread().getId() + ") FETCHED LIST");
         List<Message> fetchedMessages = unreadMessages.get(chatId);
         for (Message someMessage : fetchedMessages) {
             System.out.print(Thread.currentThread().getId() + ") " + someMessage.getMessageId() + " : ");
@@ -103,11 +133,12 @@ public class MessageService {
                 System.out.print(tempUser.getId() + ", ");
             }
             System.out.println();
-        }
+        }*/
 
         return responseList;
     }
 
+    @Transactional
     public synchronized boolean isUnreadMessagesExist(User someUser, Integer chatId) {
         List<Message> chatMessagesList = messageRepository.findAllByChatId(chatId, Sort.by(Sort.Direction.DESC, "sendTime"));
         for (Message someMessage : chatMessagesList) {
