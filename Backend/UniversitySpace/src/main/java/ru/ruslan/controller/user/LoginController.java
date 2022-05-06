4488f4361cd370a429d3253d53b75d9782b44b6e
Ruslan815 <ruslansaf2000@gmail.com>
2022-05-06 15:06:35 +0700

    Frontend User package structure was refactored

--- Backend/UniversitySpace/src/main/java/ru/ruslan/controller/user/LoginController.java
+++ Backend/UniversitySpace/src/main/java/ru/ruslan/controller/user/LoginController.java
@@ -8,6 +8,6 @@ public class LoginController {
 
     @GetMapping("/login")
     public String getLoginPage() {
-        return "html/login";
+        return "html/user/login";
     }
 }
--- Backend/UniversitySpace/src/main/java/ru/ruslan/controller/user/RegistrationController.java
+++ Backend/UniversitySpace/src/main/java/ru/ruslan/controller/user/RegistrationController.java
@@ -22,7 +22,7 @@ public class RegistrationController {
     public String registration(Model model) {
         model.addAttribute("userForm", new User());
 
-        return "html/registration";
+        return "html/user/registration";
     }
 
     @PostMapping("/registration")
