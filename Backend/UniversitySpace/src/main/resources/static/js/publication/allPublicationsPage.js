2a4cdc6862d28e516613b654f538f186699f118b
Ruslan815 <ruslansaf2000@gmail.com>
2022-05-23 21:52:42 +0700

    Created frontend page with CSS for allPublicationsPage

--- Backend/UniversitySpace/src/main/resources/static/js/publication/allPublicationsPage.js
+++ Backend/UniversitySpace/src/main/resources/static/js/publication/allPublicationsPage.js
@@ -10,16 +10,8 @@ function getAllPublicationsList(elem) {
 
     var obj = JSON.parse(message);
     allPublicationsList = obj;
-    for (var x of obj) {
-        var a = document.createElement('a');
-        var linkText = document.createTextNode(x.title);
-        a.appendChild(linkText);
-        a.title = x.title;
-        a.href = "http://localhost:8080/publication?publicationId=" + x.publicationId;
-
-        let publicationElem = document.createElement('li');
-        publicationElem.append(a);
-        elem.append(publicationElem);
+    for (var x of obj) {     
+        elem.append(createPublicationElement(x));
     }
 }
 
@@ -30,28 +22,51 @@ function searchByInput() {
     }
 
     var elem = document.getElementById("searchResult");
-    elem.removeAttribute("hidden");
+    elem.style.display = 'grid';
     elem.innerHTML = "";
-    document.getElementById("publicationsList").setAttribute("hidden", "hidden");
+    document.getElementById("publicationsList").style.display = 'none'
 
     for (var x of allPublicationsList) {
         if (x.title.toLowerCase().includes(searchString)) {
-            var a = document.createElement('a');
-            var linkText = document.createTextNode(x.title);
-            a.appendChild(linkText);
-            a.title = x.title;
-            a.href = "http://localhost:8080/publication?publicationId=" + x.publicationId;
-
-            let publicationElem = document.createElement('li');
-            publicationElem.append(a);
-            elem.append(publicationElem);
+            elem.append(createPublicationElement(x));
         }
     }
 }
 
 function clearSearchResult() {
     document.getElementById('searchInput').value = "";
-    document.getElementById("searchResult").setAttribute("hidden", "hidden");
+    document.getElementById("searchResult").style.display = 'none';
     document.getElementById("searchResult").innerHTML = "";
-    document.getElementById("publicationsList").removeAttribute("hidden");
+    document.getElementById("publicationsList").style.display = 'grid'
+}
+
+function createPublicationElement(x) {
+    var articleElem = document.createElement("article");
+    articleElem.setAttribute("class", "text");
+
+    var headerElem = document.createElement("h3");
+    if (x.title.length > 30) {
+        headerElem.innerHTML = x.title.substring(0,30) + "...";
+    } else {
+        headerElem.innerHTML = x.title;
+    }
+    
+    var textElem = document.createElement("p");
+    if (x.content.length > 150) {
+        textElem.innerHTML = x.content.substring(0,150) + "...";
+    } else {
+        textElem.innerHTML = x.content;
+    }
+    
+    var linkElem = document.createElement("a");
+    var linkText = document.createTextNode("Читать");
+    linkElem.appendChild(linkText);
+    linkElem.title = "Читать";
+    linkElem.href = "http://localhost:8080/publication?publicationId=" + x.publicationId;
+
+    articleElem.append(headerElem);
+    articleElem.append(textElem);
+    articleElem.append(linkElem);
+
+    return articleElem;
 }
